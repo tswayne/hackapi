aa818e22b5c4b1387f69a3974a08704c2958de65
Tyler Swayne <swaynetyler@gmail.com>
2017-05-20 22:11:01 -0700

    fix feed

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -2,10 +2,10 @@ const search = require('../lib/search')
 const mongo = require('mongodb');
 
 module.exports.getFeed = function(req, res) {
-  const id = new mongo.ObjectID(req.params.id);
+  const id = new mongo.ObjectID(req.params.customerId);
   const collection = require('../config/db').db.collection('customers');
   collection.find({_id: id}).toArray(function(err, docs) {
-    const customer = docs;
+    const customer = docs[0];
     const query = {tags: customer.tags};
     return search(query, (err, articles) => {
       if (err) {return res.json({error: err})}
